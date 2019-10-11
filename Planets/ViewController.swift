b6bd232dc29f4a12529f4b4777f648e6471d5990
Ege Sucu <egesucu@icloud.com>
2019-10-11 11:34:28 +0300

    Initial Commit

---
+++ Planets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Planets
+//
+//  Created by Ege Sucu on 11.10.2019.
+//  Copyright © 2019 Ege Sucu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
